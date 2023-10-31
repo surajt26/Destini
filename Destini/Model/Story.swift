897f02e328d5eeeb8d848c0cd1a276fdfc0b4435
Suraj_KY <face.surajyadav@gmail.com>
2023-10-31 09:54:32 +0530

    Structure Ready and Assets Added

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Suraj KY on 31/10/23.
+//
+
+import Foundation
---
+++ Destini/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by Suraj KY on 31/10/23.
+//
+
+import Foundation
